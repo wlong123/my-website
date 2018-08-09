dd36f454368adbc1d61bbb977a8a0cec70c60cdf
wlong123 <wlong2017@pingry.org>
2018-08-08 21:25:25 -0400

    got rid of hash strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '../../node_modules/@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
 import { SubmitComponent } from './submit/submit.component';
 import { HomeComponent } from './home/home.component';
@@ -8,11 +7,12 @@ import { HomeComponent } from './home/home.component';
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'submit', component: SubmitComponent }
+    { path: 'submit', component: SubmitComponent },
+    { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
